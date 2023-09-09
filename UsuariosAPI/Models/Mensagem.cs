4a3a1d3f737d94976fd36c6cbbae255d83023f31
grbalmeida <g.r.almeida@live.com>
2023-09-09 10:33:34 -0300

    Compondo o e-mail

--- UsuariosAPI/Models/Mensagem.cs
+++ UsuariosAPI/Models/Mensagem.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using MimeKit;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace UsuariosAPI.Models
 {
     public class Mensagem
     {
-        public List<?> Destinatario { get; set; }
+        public List<MailboxAddress> Destinatarios { get; set; }
         public string Assunto { get; set; }
         public string Conteudo { get; set; }
 
         public Mensagem(IEnumerable<string> destinatarios, string assunto, int usuarioId, string codigo)
         {
-            Destinatario = ??;
+            Destinatarios = new List<MailboxAddress>();
+            Destinatarios.AddRange(destinatarios.Select(d => new MailboxAddress(d)));
             Assunto = assunto;
             Conteudo = $"http://localhost:6000/ativa?UsuarioId={usuarioId}&CodigoDeAtivacao={codigo}";
         }
--- UsuariosAPI/Services/EmailService.cs
+++ UsuariosAPI/Services/EmailService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
 using System.Threading.Tasks;
 using UsuariosAPI.Models;
 
@@ -9,6 +11,44 @@ namespace UsuariosAPI.Services
         public async Task EnviarEmail(string[] destinarios, string assunto, int usuarioId, string code)
         {
             var mensagem = new Mensagem(destinarios, assunto, usuarioId, code);
+            var mensagemDeEmail = CriaCorpoDoEmail(mensagem);
+            Enviar(mensagemDeEmail);
+        }
+
+        private void Enviar(MimeMessage mensagemDeEmail)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect("Conexão a fazer");
+                    // TODO: Auth de e-mail
+                    client.Send(mensagemDeEmail);
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
+            }
+        }
+
+        private MimeMessage CriaCorpoDoEmail(Mensagem mensagem)
+        {
+            var mensagemDeEmail = new MimeMessage();
+            mensagemDeEmail.From.Add(new MailboxAddress("ADICIONAR O REMETENTE"));
+            mensagemDeEmail.To.AddRange(mensagem.Destinatarios);
+            mensagemDeEmail.Subject = mensagem.Assunto;
+            mensagemDeEmail.Body = new TextPart(MimeKit.Text.TextFormat.Text)
+            {
+                Text = mensagem.Conteudo
+            };
+
+            return mensagemDeEmail;
         }
     }
 }
